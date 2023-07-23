c7e76ffb414c17015b5da9cdfdb6202a3e652604
Martin Muriuki <helheim254@gmail.com>
2023-07-23 20:02:44 +0300

    Import About component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
+import About from "./About";
 
 console.log(blogData);
 
 function App() {
   const blogName = 'My First Blog';
+  const aboutText = 'Welcome to the blog. I hope you find the information helpful'
   return (
     <div className="App">
       <Header blogName={blogName} />
+      <About aboutText={aboutText} />
     </div>
   );
 }
